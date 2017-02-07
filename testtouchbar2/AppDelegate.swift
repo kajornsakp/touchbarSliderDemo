4a54fa43bd1a8a673f9855a820cf5db1cad38f76
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2017-02-07 20:16:41 +0700

    Initial Commit

---
+++ testtouchbar2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  testtouchbar2
+//
+//  Created by Kajornsak Peerapathananont on 2/7/2560 BE.
+//  Copyright © 2560 Kajornsak Peerapathananont. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ testtouchbar2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testtouchbar2
+//
+//  Created by Kajornsak Peerapathananont on 2/7/2560 BE.
+//  Copyright © 2560 Kajornsak Peerapathananont. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
